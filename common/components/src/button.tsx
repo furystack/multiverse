783a64c837c59b0b1b9b7d32842447373f51db36
Gallay Lajos <gallay.lajos@gmail.com>
2019-11-23 21:19:48 +0100

    feat(common-components):button,input

--- common/components/src/button.tsx
+++ common/components/src/button.tsx
@@ -1,13 +1,30 @@
-import { Shade, createComponent } from "@furystack/shades";
+import { Shade, createComponent, PartialElement } from "@furystack/shades";
 
-export interface ShadeButtonProps {
-  title: string;
-  onClick: (ev: MouseEvent) => void;
-}
-
-export const Button = Shade<ShadeButtonProps>({
+export const Button = Shade<PartialElement<HTMLButtonElement>>({
   shadowDomName: "shade-button",
-  render: ({ props }) => {
-    return <button onclick={props.onClick}>{props.title}</button>;
+  render: ({ props, element, children }) => {
+    element.onmouseover = () => {
+      (element.firstElementChild as HTMLElement).style.backgroundColor =
+        "rgba(0,0,0,0.1";
+    };
+    element.onmouseout = () => {
+      (element.firstElementChild as HTMLElement).style.background =
+        "rgba(0,0,0,0.05)";
+    };
+    return (
+      <button
+        {...props}
+        style={{
+          background: "rgba(0,0,0,0.05)",
+          cursor: "pointer",
+          border: "none",
+          padding: "12px 20px",
+          transition: "background .41s linear",
+          ...props.style
+        }}
+      >
+        {children}
+      </button>
+    );
   }
 });
--- frontends/wrap-r/src/pages/login.tsx
+++ frontends/wrap-r/src/pages/login.tsx
@@ -1,5 +1,5 @@
 import { Shade, createComponent } from "@furystack/shades";
-import { Button } from "common-components";
+import { Button, Input } from "common-components";
 import { SessionService } from "../services/session";
 import { Loader } from "../components/loader";
 
@@ -41,48 +41,13 @@ export const Login = Shade({
         }}
       >
         <style>{`
-          label {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            justify-content: space-between;
-            font-size: 10px;
-            color: #999;
-            margin-bottom: 1em;
-            padding: 1em;
-            border-radius: 5px;
-            transition: background-color 300ms cubic-bezier(0.455, 0.030, 0.515, 0.955), box-shadow 300ms cubic-bezier(0.455, 0.030, 0.515, 0.955);
-          }
-
-          label:focus-within {
-            background-color: rgba(255,255,255,0.2);
-            box-shadow: 0px 0px 5px rgba(0,0,0,.1)
-          }
-
-          label input{
-            border: none;
-            background-color: transparent;
-            outline: none;
-            font-size: 12px;
-          }
-
+          
           form.login-form {
             border: 1px solid #aaa;
             padding: 10px 30px;
             border-radius: 8px;
             box-shadow: 1px 1px 3px rgba(0,0,0,0.3);
             background-color: rgba(255,255,255,0.4);
-          }
-
-          button.login-button {
-            background: rgba(0,0,0,0.05);
-            cursor: pointer;
-            border: none;
-            padding: 12px 20px;
-          }
-
-          button.login-button:hover:not(:disabled){
-            background: rgba(0,0,0,0.25);
           }
 
           h2 {
@@ -99,42 +64,38 @@ export const Login = Shade({
           }}
         >
           <h2>Login</h2>
-          <label>
-            Username
-            <input
-              required
-              disabled={getState().isOperationInProgress}
-              placeholder="The user's login name"
-              value={username}
-              onchange={ev => {
-                updateState(
-                  {
-                    username: (ev.target as HTMLInputElement).value
-                  },
-                  true
-                );
-              }}
-              type="text"
-            />
-          </label>
-          <label>
-            Password
-            <input
-              required
-              disabled={getState().isOperationInProgress}
-              placeholder="The password for the user"
-              value={password}
-              type="password"
-              onchange={ev => {
-                updateState(
-                  {
-                    password: (ev.target as HTMLInputElement).value
-                  },
-                  true
-                );
-              }}
-            />
-          </label>
+          <Input
+            labelTitle="Username"
+            required
+            disabled={getState().isOperationInProgress}
+            placeholder="The user's login name"
+            value={username}
+            onchange={ev => {
+              updateState(
+                {
+                  username: (ev.target as HTMLInputElement).value
+                },
+                true
+              );
+            }}
+            type="text"
+          />
+          <Input
+            labelTitle="Password"
+            required
+            disabled={getState().isOperationInProgress}
+            placeholder="The password for the user"
+            value={password}
+            type="password"
+            onchange={ev => {
+              updateState(
+                {
+                  password: (ev.target as HTMLInputElement).value
+                },
+                true
+              );
+            }}
+          />
           <div
             style={{
               display: "flex",
@@ -149,11 +110,7 @@ export const Login = Shade({
             ) : (
               <div />
             )}
-            <button
-              className="login-button"
-              disabled={getState().isOperationInProgress}
-              type="submit"
-            >
+            <Button disabled={getState().isOperationInProgress} type="submit">
               <div
                 style={{
                   display: "flex",
@@ -171,18 +128,13 @@ export const Login = Shade({
                   />
                 ) : null}
               </div>
-            </button>
+            </Button>
           </div>
           <p style={{ fontSize: "10px" }}>
             You can login with the default 'testuser' / 'password' credentials
           </p>
         </form>
-        <Button
-          title="alma"
-          onClick={() => {
-            alert("ALMAAA");
-          }}
-        />
+        <Input type="text" labelTitle="Alma" />
       </div>
     );
   }
