848d8ffde49f6169c05e69d2d0ba70c587c0d209
Lajos Gallay <gallay.lajos@gmail.com>
2020-03-19 11:05:46 +0100

    chore(config):db url env vars

--- common/config/src/databases.ts
+++ common/config/src/databases.ts
@@ -1,15 +1,15 @@
 export const databases = {
   'common-auth': {
-    mongoUrl: 'mongodb://localhost:27017',
+    mongoUrl: process.env.MONGO_COMMON_AUTH_URL || process.env.MONGO_URL || 'mongodb://localhost:27017',
     dbName: 'multiverse-common-auth',
     usersCollection: 'users',
     sessionStore: {
-      host: 'localhost',
-      port: '63790',
+      host: process.env.SESSION_STORE_HOST || process.env.REDIS_HOST || 'localhost',
+      port: process.env.SESSION_STORE_PORT || process.env.REDIS_PORT || '63790',
     },
   },
   logging: {
-    mongoUrl: 'mongodb://localhost:27017',
+    mongoUrl: process.env.MONGO_LOGGING_URL || process.env.MONGO_URL || 'mongodb://localhost:27017',
     dbName: 'multiverse-logs',
     logCollection: 'entries',
   },
