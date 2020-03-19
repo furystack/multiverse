35b98bdd3734c6150f69ac2b3464746993c37e68
Gallay Lajos <gallay.lajos@gmail.com>
2020-03-19 12:00:41 +0100

    fix(config): modified sites to listen all hosts

--- common/config/src/sites.ts
+++ common/config/src/sites.ts
@@ -1,9 +1,9 @@
 export const sites = {
   services: {
-    'wrap-r': process.env.WRAPPR_SERVICE_URL || 'http://localhost:9090',
-    'logg-r': process.env.LOGGR_SERVICE_URL || 'http://localhost:9091',
+    'wrap-r': process.env.WRAPPR_SERVICE_URL || 'http://0.0.0.0:9090',
+    'logg-r': process.env.LOGGR_SERVICE_URL || 'http://0.0.0.0:9091',
   },
   frontends: {
-    'wrap-r': process.env.WRAPPR_FRONTEND_URL || 'http://localhost:8080',
+    'wrap-r': process.env.WRAPPR_FRONTEND_URL || 'http://0.0.0.0:8080',
   },
 }
