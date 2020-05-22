f39714b2d8e54d28f1b50dcc1c136ed32d81d9d9
Gallay Lajos <gallay.lajos@gmail.com>
2020-05-22 17:48:14 +0200

    test(cypress):tryfix

--- cypress/integration/signup.spec.ts
+++ cypress/integration/signup.spec.ts
@@ -34,6 +34,6 @@ describe('Sign up', () => {
     cy.get('input[required][title=Password]').should('be.visible').type('asdasd123')
     cy.get('input[required][title="Confirm password"]').should('be.visible').type('asdasd123')
     cy.get('button[title=Register]').click()
-    cy.get('h2').contains('Failed to sign up :(').should('be.visible')
+    cy.contains('Failed to sign up :(').should('be.visible')
   })
 })
