e01c3ab1e75767bcb7ed0b9a2da48c8626167b45
Gallay Lajos <gallay.lajos@gmail.com>
2020-06-22 22:37:39 +0200

    fix(media):upload-encoded stores

--- services/media/src/actions/upload-encoded.ts
+++ services/media/src/actions/upload-encoded.ts
@@ -4,6 +4,7 @@ import { RequestAction, JsonResult, RequestError } from '@furystack/rest'
 import { IncomingForm, Fields, Files } from 'formidable'
 import { FileStores } from '@common/config'
 import { media } from '@common/models'
+import { StoreManager } from '@furystack/core'
 
 export const UploadEncoded: RequestAction<{ urlParams: { movieId: string; accessToken: string } }> = async ({
   injector,
@@ -12,14 +13,16 @@ export const UploadEncoded: RequestAction<{ urlParams: { movieId: string; access
 }) => {
   const { movieId, accessToken } = getUrlParams()
 
-  const [job] = await injector
-    .getDataSetFor(media.EncodingTask)
-    .find(injector, { filter: { authToken: { $eq: accessToken } }, top: 1 })
+  const storeManager = injector.getInstance(StoreManager)
+
+  const [job] = await storeManager
+    .getStoreFor(media.EncodingTask)
+    .find({ filter: { authToken: { $eq: accessToken } }, top: 1 })
   if (!job) {
     throw new RequestError('Unauthorized', 401)
   }
 
-  const movie = await injector.getDataSetFor(media.Movie).get(injector, movieId)
+  const movie = await storeManager.getStoreFor(media.Movie).get(movieId)
   if (!movie) {
     throw new RequestError('Movie not found', 404)
   }
