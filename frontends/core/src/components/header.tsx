db8f8db4811c9dd8efde1581cd4e7beadb421d38
Gallay Lajos <gallay.lajos@gmail.com>
2020-06-23 16:48:29 +0200

    feat(build): added pm2

--- frontends/core/src/components/header.tsx
+++ frontends/core/src/components/header.tsx
@@ -16,7 +16,7 @@ export const Header = Shade({
             textOverflow: 'ellipsis',
           }}>
           <RouteLink title="🌀 Multiverse" href="/" style={{ marginRight: '1em', cursor: 'pointer' }}>
-            🌀 Multiverse
+            🌀 Multiverse Alma
           </RouteLink>
           <Router
             notFound={() => <div />}
