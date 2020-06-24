f8b3c7ef563a50402ec366509268c50da4a74f63
Gallay Lajos <gallay.lajos@gmail.com>
2020-06-24 11:53:30 +0200

    build(azure,config,nginx): from ubuntu to pi, more slim install

---
+++ remove-frontend-workspaces.js
@@ -0,0 +1,17 @@
+const { promises } = require('fs')
+const rimraf = require('rimraf')
+
+const run = async () => {
+  console.log("Removing './frontend/*' from 'package.json/workspaces/packages'...")
+  const package = require('./package.json')
+  package.workspaces.packages = package.workspaces.packages.filter((p) => p !== 'frontends/*')
+  promises.writeFile(JSON.stringify(package, undefined, 2))
+
+  console.log("Removing './common/components'")
+  await new Promise((resolve, reject) => rimraf('./common/components', (err) => (err ? reject(err) : resolve())))
+
+  console.log("Removing './common/frontend-utils'")
+  await new Promise((resolve, reject) => rimraf('./common/frontend-utils', (err) => (err ? reject(err) : resolve())))
+}
+
+run()
