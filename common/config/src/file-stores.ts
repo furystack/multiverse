4dac22a57706a13abc32357f78498da3c1f5f8a5
Gallay Lajos <gallay.lajos@gmail.com>
2020-06-24 09:56:52 +0200

    fix(file-stores): default data paths

--- common/config/src/file-stores.ts
+++ common/config/src/file-stores.ts
@@ -1,6 +1,6 @@
 export const FileStores = {
-  avatars: process.env.FILE_STORE_AVATARS_PATH || '/data/avatars',
-  encodedMedia: process.env.FILE_STORE_ENCODED_MEDIA || '/data/encoded_media',
+  avatars: process.env.FILE_STORE_AVATARS_PATH || '~/multiverse-data/avatars',
+  encodedMedia: process.env.FILE_STORE_ENCODED_MEDIA || '~/multiverse-data/encoded_media',
   mediaEncoderWorkerTemp: process.env.MEDIA_ENCODER_WORKER_TEMP || 'd:\\temp\\ffmpeg-mock', // ToDo: fixme :)
   tempdir: process.env.TEMP || '/tmp',
 }
