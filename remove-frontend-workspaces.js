109feaa1ad11ae7b2bf94e99fefa85814cce3608
Gallay Lajos <gallay.lajos@gmail.com>
2020-06-24 12:55:07 +0200

    fix(build):slimmer install

--- remove-frontend-workspaces.js
+++ remove-frontend-workspaces.js
@@ -5,7 +5,7 @@ const run = async () => {
   console.log("Removing './frontend/*' from 'package.json/workspaces/packages'...")
   const package = require('./package.json')
   package.workspaces.packages = package.workspaces.packages.filter((p) => p !== 'frontends/*')
-  promises.writeFile(JSON.stringify(package, undefined, 2))
+  promises.writeFile('package.json', JSON.stringify(package, undefined, 2))
 
   console.log("Removing './common/components'")
   await new Promise((resolve, reject) => rimraf('./common/components', (err) => (err ? reject(err) : resolve())))
