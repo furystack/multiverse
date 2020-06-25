63dd22afdff1ac25f064d9823046f66556602e6a
Lajos Gallay <gallay.lajos@gmail.com>
2020-06-25 17:16:17 +0200

    feat(sites):optional fallback to API root

--- common/config/src/sites.ts
+++ common/config/src/sites.ts
@@ -2,19 +2,23 @@ export const sites = {
   services: {
     auth: {
       internalPort: process.env.AUTH_SERVICE_INTERNAL_PORT || 9090,
-      externalPath: process.env.AUTH_SERVICE_EXTERNAL_URL || 'http://localhost:9090/api',
+      externalPath:
+        process.env.AUTH_SERVICE_EXTERNAL_URL || process.env.EXTERNAL_SERVICE_URL || 'http://localhost:9090/api',
     },
     diag: {
       internalPort: process.env.DIAG_SERVICE_INTERNAL_PORT || 9091,
-      externalPath: process.env.DIAG_SERVICE_EXTERNAL_URL || 'http://localhost:9091/api',
+      externalPath:
+        process.env.DIAG_SERVICE_EXTERNAL_URL || process.env.EXTERNAL_SERVICE_URL || 'http://localhost:9091/api',
     },
     xpense: {
       internalPort: process.env.XPENSE_SERVICE_INTERNAL_PORT || 9092,
-      externalPath: process.env.XPENSE_SERVICE_EXTERNAL_URL || 'http://localhost:9092/api',
+      externalPath:
+        process.env.XPENSE_SERVICE_EXTERNAL_URL || process.env.EXTERNAL_SERVICE_URL || 'http://localhost:9092/api',
     },
     media: {
       internalPort: process.env.MEDIA_SERVICE_INTERNAL_PORT || 9093,
-      externalPath: process.env.MEDIA_SERVICE_EXTERNAL_URL || 'http://localhost:9093/api',
+      externalPath:
+        process.env.MEDIA_SERVICE_EXTERNAL_URL || process.env.EXTERNAL_SERVICE_URL || 'http://localhost:9093/api',
     },
   },
   frontends: {
