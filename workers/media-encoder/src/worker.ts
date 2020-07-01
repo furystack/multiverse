de97af78a4350602a9120c8219ccb5a6d50734ae
Gallay Lajos <gallay.lajos@gmail.com>
2020-07-01 19:35:54 +0200

    fix(media-encoder): temp dir check before taking task

--- workers/media-encoder/src/worker.ts
+++ workers/media-encoder/src/worker.ts
@@ -1,13 +1,28 @@
 // import { argv } from 'process'
-import { attachShutdownHandler } from '@common/service-utils'
+import { attachShutdownHandler, existsAsync } from '@common/service-utils'
 import { VerboseConsoleLogger } from '@furystack/logging'
 import { Injector } from '@furystack/inject'
+import { FileStores } from '@common/config'
 import { RabbitListener } from './services/rabbit-listener'
 
 // const namePostfix = argv[2] || 'default'
 
 export const injector = new Injector()
 injector.useLogging(VerboseConsoleLogger)
-injector.logger.withScope('INIT').information({ message: 'Initializing media-encoder...' })
+
+const logger = injector.logger.withScope('INIT')
+;(async () => {
+  await logger.information({ message: 'Initializing media-encoder...' })
+  const tempDirExists = await existsAsync(FileStores.mediaEncoderWorkerTemp)
+
+  if (!tempDirExists) {
+    logger.error({
+      message: 'Media Worker temp dir does not exists or not accessible. Task skipped!',
+      data: { dir: FileStores.mediaEncoderWorkerTemp },
+    })
+    process.exit(1)
+  }
+})()
+
 injector.getInstance(RabbitListener)
 attachShutdownHandler(injector)
