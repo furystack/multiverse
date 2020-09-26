ebdd6db643589bf32bd1c24b2474617afa57ed96
Gallay Lajos <gallay.lajos@gmail.com>
2020-09-26 14:55:24 +0200

    feat(media): added AVI to available movie extensions

--- services/media/src/utils/is-movie-file.ts
+++ services/media/src/utils/is-movie-file.ts
@@ -1,6 +1,6 @@
 export const isMovieFile = (path: string) => {
   const pathToLower = path.toLowerCase()
-  if (pathToLower.endsWith('.mkv') || pathToLower.endsWith('.webm')) {
+  if (pathToLower.endsWith('.mkv') || pathToLower.endsWith('.webm') || pathToLower.endsWith('.avi')) {
     return true
   }
   return false
