bf3fae6f529d1758819652ae6d8b78c716781f86
Gallay Lajos <gallay.lajos@gmail.com>
2020-11-26 21:09:57 +0100

    fix(common/config): removed os import

--- common/config/src/file-stores.ts
+++ common/config/src/file-stores.ts
@@ -1,8 +1,7 @@
-import { tmpdir } from 'os'
 export const FileStores = {
   avatars: process.env.FILE_STORE_AVATARS_PATH || '/tmp/multiverse-data/avatars',
   encodedMedia: process.env.FILE_STORE_ENCODED_MEDIA || '/tmp/multiverse-data/encoded_media',
   subtitles: process.env.FILE_STORE_SUBTITLES || '/tmp/multiverse-data/encoded_media',
   mediaEncoderWorkerTemp: process.env.MEDIA_ENCODER_WORKER_TEMP || '/tmp/multiverse-data/worker-temp',
-  tempdir: process.env.TEMP || tmpdir(),
+  tempdir: process.env.TEMP || '/tmp',
 }
