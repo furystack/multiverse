f992d24f961ce7856005b59a9dfc818fbf0df09b
Gallay Lajos <gallay.lajos@gmail.com>
2020-12-06 15:06:23 +0100

    minor cleanup, increased initial Cypress timeout

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -4,9 +4,13 @@ export const login = (cy: Cypress.cy, username: string, password: string) => {
   const loginButtonSelector = 'shade-login button[type=submit]'
   const usernameInputSelector = 'shade-login input[type=text][title=username]'
   const passwordFieldSelector = 'shade-login input[type=password]'
-  cy.visit('/')
-  cy.get(usernameInputSelector).type(username).blur()
-  cy.get(passwordFieldSelector).type(password).blur()
+  cy.visit('/', { timeout: 120000 })
+  cy.get(usernameInputSelector)
+    .type(username)
+    .blur()
+  cy.get(passwordFieldSelector)
+    .type(password)
+    .blur()
   cy.log('Logging in...')
   cy.get(loginButtonSelector).click()
 }
@@ -22,5 +26,8 @@ export const logoutFromUserMenu = (cy: Cypress.cy) => {
 
 export const navigateFromUserMenu = (cy: Cypress.cy, app: typeof serviceNames[number]) => {
   openUserMenu(cy)
-  cy.get(`shade-current-user-menu`).contains(app).scrollIntoView().click()
+  cy.get(`shade-current-user-menu`)
+    .contains(app)
+    .scrollIntoView()
+    .click()
 }
