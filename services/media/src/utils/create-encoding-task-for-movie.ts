abae8a3f80b05547136dc5cd33d5af0efb88b04b
Gallay Lajos <gallay.lajos@gmail.com>
2020-12-09 13:32:52 +0100

    feat(media): clean encode folder before re-encode

--- services/media/src/utils/create-encoding-task-for-movie.ts
+++ services/media/src/utils/create-encoding-task-for-movie.ts
@@ -1,7 +1,11 @@
+import { join } from 'path'
 import { media } from '@common/models'
 import { Injector } from '@furystack/inject'
 import { StoreManager } from '@furystack/core'
 import { v4 } from 'uuid'
+import { FileStores } from '@common/config'
+import rimraf from 'rimraf'
+import { existsAsync } from '@common/service-utils'
 import { MediaMessaging } from '../services/media-messaging'
 
 export const createEncodingTaskForMovie = async ({ movie, injector }: { movie: media.Movie; injector: Injector }) => {
@@ -20,6 +24,24 @@ export const createEncodingTaskForMovie = async ({ movie, injector }: { movie: m
     logger.verbose({ message: 'Encoding has been disabled for the Movie Library, skipping...' })
     return
   }
+  const targetPath = join(FileStores.encodedMedia, library.encoding.codec, library.encoding.mode, movie._id)
+  const targetExists = await existsAsync(targetPath)
+  if (targetExists) {
+    await new Promise<void>((resolve, reject) => rimraf(targetPath, {}, (err) => (err ? reject(err) : resolve())))
+    await logger.verbose({
+      message: `The previous encoding data was cleaned up for encoding movie '${movie.metadata.title}' in ${targetPath}`,
+      data: {
+        targetPath,
+        codec: library.encoding.codec,
+        mode: library.encoding.mode,
+        movie: {
+          _id: movie._id,
+          title: movie.metadata.title,
+        },
+      },
+    })
+  }
+
   return await injector.getDataSetFor(media.EncodingTask).add(injector, {
     authToken: v4(),
     percent: 0,
