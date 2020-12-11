91f4a09cc135d558879aa7257b97825f91823409
Gallay Lajos <gallay.lajos@gmail.com>
2020-12-11 12:19:30 +0100

    lint(project): removed prettier

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -11,36 +11,24 @@ describe('Core Application', () => {
   describe('Login with username / password', () => {
     it('Should show proper error message for invalid credentials', () => {
       cy.visit('/')
-      cy.get(usernameInputSelector)
-        .type('wrongUser:(')
-        .blur()
-      cy.get(passwordFieldSelector)
-        .type('wrongPassword')
-        .blur()
+      cy.get(usernameInputSelector).type('wrongUser:(').blur()
+      cy.get(passwordFieldSelector).type('wrongPassword').blur()
       cy.get(loginButtonSelector).click()
       expectAndDismissNotification(cy, 'Please check your credentials', 'warning')
-      cy.get(loginErrorSelector)
-        .should('be.visible')
-        .contains('Login Failed')
+      cy.get(loginErrorSelector).should('be.visible').contains('Login Failed')
     })
 
     it('Should redirect to Accept Terms when not accepted', () => {
       cy.visit('/')
-      cy.get(usernameInputSelector)
-        .type('testTermsNotAcceptedUser@gmail.com')
-        .blur()
-      cy.get(passwordFieldSelector)
-        .type('password')
-        .blur()
+      cy.get(usernameInputSelector).type('testTermsNotAcceptedUser@gmail.com').blur()
+      cy.get(passwordFieldSelector).type('password').blur()
       cy.get(loginButtonSelector).click()
       cy.get('welcome-page multiverse-dashboard > div').should('not.exist')
 
       cy.get('accept-terms-page')
         .should('be.visible')
         .within(() => {
-          cy.contains('accept')
-            .scrollIntoView()
-            .click()
+          cy.contains('accept').scrollIntoView().click()
         })
       expectAndDismissNotification(cy, 'You have accepted the terms', 'success')
       cy.get('welcome-page multiverse-dashboard > div').should('be.visible')
@@ -53,18 +41,10 @@ describe('Core Application', () => {
       cy.get(loginFormSelector).toMatchImageSnapshot({ threshold: 0.001 })
 
       cy.get(headerSelector).toMatchImageSnapshot({ threshold: 0.001 }) // with no avatar
-      cy.get(usernameInputSelector)
-        .should('be.visible')
-        .should('be.enabled')
-      cy.get(usernameInputSelector)
-        .type('testuser@gmail.com')
-        .blur()
-      cy.get(passwordFieldSelector)
-        .should('be.visible')
-        .should('be.enabled')
-      cy.get(passwordFieldSelector)
-        .type('password')
-        .blur()
+      cy.get(usernameInputSelector).should('be.visible').should('be.enabled')
+      cy.get(usernameInputSelector).type('testuser@gmail.com').blur()
+      cy.get(passwordFieldSelector).should('be.visible').should('be.enabled')
+      cy.get(passwordFieldSelector).type('password').blur()
       cy.get(loginButtonSelector).should('be.visible')
       cy.get(loginButtonSelector).click()
 
@@ -72,18 +52,12 @@ describe('Core Application', () => {
 
       cy.get('welcome-page multiverse-dashboard > div').should('be.visible')
 
-      cy.get(headerSelector)
-        .should('be.visible')
-        .toMatchImageSnapshot({ threshold: 0.001 }) // with avatar and menu
+      cy.get(headerSelector).should('be.visible').toMatchImageSnapshot({ threshold: 0.001 }) // with avatar and menu
       logoutFromUserMenu(cy)
 
       cy.get(loginFormSelector).should('be.visible')
-      cy.get(usernameInputSelector)
-        .should('be.visible')
-        .should('be.empty')
-      cy.get(passwordFieldSelector)
-        .should('be.visible')
-        .should('be.empty')
+      cy.get(usernameInputSelector).should('be.visible').should('be.empty')
+      cy.get(passwordFieldSelector).should('be.visible').should('be.empty')
     })
   })
 
@@ -92,13 +66,9 @@ describe('Core Application', () => {
 
     it('Should display a proper error message for wrong code', () => {
       cy.visit('/')
-      cy.get(githubLoginButtonSelector)
-        .should('be.visible')
-        .should('be.enabled')
+      cy.get(githubLoginButtonSelector).should('be.visible').should('be.enabled')
       cy.visit('/github-login?code=123456')
-      cy.get('button')
-        .should('be.visible')
-        .should('be.enabled')
+      cy.get('button').should('be.visible').should('be.enabled')
       cy.get('shade-github-login').contains('There was an error during Github login')
     })
 
@@ -121,10 +91,7 @@ describe('Core Application', () => {
     const googleLoginButtonSelector = 'shade-button button[title=Google]'
     it('Should display a correct error message on invalid login', () => {
       cy.visit('/')
-      cy.get(googleLoginButtonSelector)
-        .should('be.visible')
-        .should('be.enabled')
-        .click()
+      cy.get(googleLoginButtonSelector).should('be.visible').should('be.enabled').click()
     })
     it.skip('Log in and logout roundtrip', () => {
       /** */
