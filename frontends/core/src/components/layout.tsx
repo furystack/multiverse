98ab261411d49bf420ec566683b2b785fbb23532
Gallay Lajos <gallay.lajos@gmail.com>
2020-12-16 10:02:55 +0100

    fix(frontend): Partial CSS declaration types

--- frontends/core/src/components/layout.tsx
+++ frontends/core/src/components/layout.tsx
@@ -1,4 +1,4 @@
-import { createComponent, Shade, Router, LazyLoad, PartialElement } from '@furystack/shades'
+import { createComponent, Shade, Router, LazyLoad } from '@furystack/shades'
 import { Injector } from '@furystack/inject'
 import { CommandPalette, defaultLightTheme, NotyList, ThemeProviderService } from '@furystack/shades-common-components'
 import { DocsPage } from '../pages/docs'
@@ -13,7 +13,7 @@ import { CurrentUserMenu } from './current-user-menu'
 const lightBackground = 'linear-gradient(to right bottom, #ebebf8, #e3e3f6, #dcdcf4, #d4d4f2, #cdcdf0)'
 const darkBackground = 'linear-gradient(to right bottom, #2b3036, #292c31, #27282d, #242428, #212023)'
 
-const getStyles = (injector: Injector): PartialElement<CSSStyleDeclaration> => {
+const getStyles = (injector: Injector): Partial<CSSStyleDeclaration> => {
   const themeProvider = injector.getInstance(ThemeProviderService)
   const isLight = themeProvider.theme.getValue() === defaultLightTheme
   const backgroundImage = isLight ? lightBackground : darkBackground
