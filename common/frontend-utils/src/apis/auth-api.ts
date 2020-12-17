102c115b63b902a718de1a91ece5202e21000caa
Gallay Lajos <gallay.lajos@gmail.com>
2020-12-17 11:22:14 +0100

    feat(frontend-utils, frontend-core): env service, site root urls

--- common/frontend-utils/src/apis/auth-api.ts
+++ common/frontend-utils/src/apis/auth-api.ts
@@ -3,14 +3,17 @@ import { PathHelper } from '@furystack/utils'
 import { apis, serialize } from '@common/models'
 import { createClient } from '@furystack/rest-client-fetch'
 import { sites } from '@common/config'
+import { EnvironmentService } from '../environment-service'
 
 @Injectable({ lifetime: 'singleton' })
 export class AuthApiService {
   public call = createClient<apis.AuthApi>({
-    endpointUrl: PathHelper.joinPaths(window.location.origin, sites.services.auth.apiPath),
+    endpointUrl: PathHelper.joinPaths(this.env.siteRoots.auth, sites.services.auth.apiPath),
     serializeQueryParams: serialize,
     requestInit: {
       credentials: 'include',
     },
   })
+
+  constructor(private readonly env: EnvironmentService) {}
 }
