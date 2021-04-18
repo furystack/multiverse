d7576fc514b8c9a78f9c0af9260c744c7cd26d29
Lajos Gallay <gallay@xund.ai>
2021-04-18 15:09:40 +0200

    fix(frontend-utils): fixed error msg fallback

--- common/frontend-utils/src/get-error-message.ts
+++ common/frontend-utils/src/get-error-message.ts
@@ -2,9 +2,12 @@ import { ResponseError } from '@furystack/rest-client-fetch/dist/response-error'
 
 export const getErrorMessage = async (error: Error): Promise<string> => {
   if (error instanceof ResponseError) {
-    const responseBody = await error.response.json()
-    return responseBody.message?.toString() || error.toString()
-  } else {
-    return error.toString()
+    try {
+      const responseBody = await error.response.json()
+      return responseBody.message?.toString() || error.toString()
+    } catch {
+      // failed to deserialize, fall back to error.toString()
+    }
   }
+  return error.toString()
 }
