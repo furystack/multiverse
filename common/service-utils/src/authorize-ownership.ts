415bf9cdebc7fab5c4c6227a4ff5ff8fd05394d1
Gallay Lajos <gallay.lajos@gmail.com>
2021-05-17 13:55:17 +0200

    lint(eslint): autofixes

--- common/service-utils/src/authorize-ownership.ts
+++ common/service-utils/src/authorize-ownership.ts
@@ -8,35 +8,34 @@ export interface AuthorizeOwnershipOptions {
 }
 export const AuthorizeOwnership: <T extends { owner: Owner }>(
   options: AuthorizeOwnershipOptions,
-) => (authOptions: { injector: Injector; entity: T }) => Promise<AuthorizationResult> = (options) => async ({
-  entity,
-  injector,
-}) => {
-  const usr = await injector.getCurrentUser()
-  const { owner } = entity
-  if (options.level.includes('owner') && owner.type === 'user' && owner.username === usr.username) {
-    return { isAllowed: true }
-  }
-  if (owner.type === 'organization') {
-    const [ownerOrg] = await injector
-      .getInstance(StoreManager)
-      .getStoreFor(auth.Organization, '_id')
-      .find({ top: 1, filter: { name: { $eq: owner.organizationName } } })
-    if (ownerOrg) {
-      if (
-        options.level.includes('organizationOwner') &&
-        ownerOrg.owner.type === 'user' &&
-        ownerOrg.owner.username === usr.username
-      ) {
-        return { isAllowed: true }
-      }
-      if (options.level.includes('admin') && ownerOrg.adminNames.includes(usr.username)) {
-        return { isAllowed: true }
-      }
-      if (options.level.includes('member') && ownerOrg.memberNames.includes(usr.username)) {
-        return { isAllowed: true }
+) => (authOptions: { injector: Injector; entity: T }) => Promise<AuthorizationResult> =
+  (options) =>
+  async ({ entity, injector }) => {
+    const usr = await injector.getCurrentUser()
+    const { owner } = entity
+    if (options.level.includes('owner') && owner.type === 'user' && owner.username === usr.username) {
+      return { isAllowed: true }
+    }
+    if (owner.type === 'organization') {
+      const [ownerOrg] = await injector
+        .getInstance(StoreManager)
+        .getStoreFor(auth.Organization, '_id')
+        .find({ top: 1, filter: { name: { $eq: owner.organizationName } } })
+      if (ownerOrg) {
+        if (
+          options.level.includes('organizationOwner') &&
+          ownerOrg.owner.type === 'user' &&
+          ownerOrg.owner.username === usr.username
+        ) {
+          return { isAllowed: true }
+        }
+        if (options.level.includes('admin') && ownerOrg.adminNames.includes(usr.username)) {
+          return { isAllowed: true }
+        }
+        if (options.level.includes('member') && ownerOrg.memberNames.includes(usr.username)) {
+          return { isAllowed: true }
+        }
       }
     }
+    return { isAllowed: false, message: `Not in a valid role: ${options.level.join(',')}` }
   }
-  return { isAllowed: false, message: `Not in a valid role: ${options.level.join(',')}` }
-}
