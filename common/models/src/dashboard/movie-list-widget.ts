46c5977c089cec7891aae86b19a43eab4c537d2d
Gallay Lajos <gallay.lajos@gmail.com>
2021-05-17 18:35:30 +0200

    added jsdoc

--- common/models/src/dashboard/movie-list-widget.ts
+++ common/models/src/dashboard/movie-list-widget.ts
@@ -1,6 +1,16 @@
 import { WidgetBase } from './widget-base'
 
 export interface MovieListWidget extends WidgetBase<'movie-list'> {
+  /**
+   * The Widget Title
+   */
   title: string
+  /**
+   * A list of movie IDs
+   */
   movieIds: string[]
+  /**
+   * A size of a Movie widget
+   */
+  size: number
 }
