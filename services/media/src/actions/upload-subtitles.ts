85a49fe8a66e46fb71bc7b733e9e079d4aed91d5
Gallay Lajos <gallay.lajos@gmail.com>
2021-11-09 21:24:30 +0100

    refactor(@service/media): replaced copy+unlink with rename

--- services/media/src/actions/upload-subtitles.ts
+++ services/media/src/actions/upload-subtitles.ts
@@ -54,9 +54,7 @@ export const UploadSubtitles: RequestAction<{
       .filter((f) => !(f instanceof Array) && f.originalFilename && extname(f.originalFilename) === 'vtt')
       .map(async (file) => {
         if (!(file instanceof Array)) {
-          await promises.copyFile(file.filepath, join(targetPath, file.originalFilename as string))
-          // Remove from temp
-          await promises.unlink(file.filepath)
+          await promises.rename(file.filepath, join(targetPath, file.originalFilename as string))
         }
       }),
   )
